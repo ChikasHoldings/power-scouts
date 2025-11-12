cc3a50de582614db665c0e680de4012284e8dd05
chiedu Kabakwu <chikasholdings@gmail.com>
2025-11-12 23:58:15 +0000

    File changes

--- src/components/home/ProvidersSection.jsx
+++ src/components/home/ProvidersSection.jsx
@@ -25,7 +25,6 @@ const providerLogos = [
 
 export default function ProvidersSection() {
   return (
-    {/* Trusted Electricity Providers Section */}
     <section className="py-16 bg-white">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <motion.div
