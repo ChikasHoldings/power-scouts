81eeb52b8ec12b46fa36cd635fa1c3149357d254
chiedu Kabakwu <chikasholdings@gmail.com>
2025-11-13 09:26:52 +0000

    File changes

--- src/components/home/AboutSection.jsx
+++ src/components/home/AboutSection.jsx
@@ -42,9 +42,9 @@ export default function AboutSection() {
         <div className="grid lg:grid-cols-2 gap-12 items-center">
           {/* Image */}
           <div className="order-2 lg:order-1">
-            <div className="relative rounded-xl overflow-hidden shadow-lg h-full">
+            <div className="relative rounded-xl overflow-hidden shadow-lg h-full min-h-[500px]">
               <img
-                src="https://images.unsplash.com/photo-1511895426328-dc8714191300?w=800&q=80"
+                src="https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/69141a7199585b6c94026f23/34b7db789_portrait-happy-multi-ethnic-family-embracing-adopted-kids-bonding-together.jpg"
                 alt="Happy family enjoying their home"
                 className="w-full h-full object-cover" />
             </div>
